669cc7e3f686b6d9e3c2ed09bfa7d211295c6d59
therewillbecode <tomw08@gmail.com>
2025-03-17 08:17:51 +0000

    wip

---
+++ tasks/pbt/src/lib.rs
@@ -0,0 +1,100 @@
+use oxc_ast::{NONE, ast::*};
+use oxc_ecmascript::side_effects::MayHaveSideEffects;
+use oxc_span::{ContentEq, GetSpan};
+use oxc_syntax::es_target::ESTarget;
+
+#[cfg(test)]
+mod test {
+    use oxc_allocator::{Allocator, Box, IntoIn, Vec};
+    use oxc_ast::ast::{BooleanLiteral, Expression, LogicalExpression, LogicalOperator};
+    use oxc_codegen::{CodeGenerator, CodegenOptions};
+    use oxc_span::Span;
+    use oxc_syntax::es_target::ESTarget;
+    use proptest::prelude::*;
+
+    fn bool_lit_strat() -> impl Strategy<Value = Expression<'static>> {
+        (proptest::bool::weighted(0.5)).prop_map(|x| {
+            let b = BooleanLiteral { span: Span::empty(0), value: x };
+            let alloc = Allocator::default();
+            Expression::BooleanLiteral(Box::new_in(b, &alloc))
+        })
+    }
+
+    fn logical_expr_strat() -> impl Strategy<Value = LogicalExpression<'static>> {
+        (
+            prop_oneof![Just(LogicalOperator::Or), Just(LogicalOperator::And),],
+            bool_lit_strat(),
+            bool_lit_strat(),
+        )
+            .prop_map(|(op, l, r)| {
+                // let lb = BooleanLiteral { span: Span::empty(0), value: x };
+                // let rb = BooleanLiteral { span: Span::empty(0), value: y };
+                let left: Expression = l;
+                let right: Expression = r;
+                let operator: LogicalOperator = op;
+                let span: Span = Span::empty(0);
+                let a = LogicalExpression { left, right, operator, span };
+                a
+            })
+    }
+
+    fn nested_logical_expr_strat() -> impl Strategy<Value = Expression<'static>> {
+        let leaf = prop_oneof![bool_lit_strat(),];
+        leaf.prop_recursive(
+            80,  // 8 levels deep
+            256, // Shoot for maximum size of 256 nodes
+            10,  // We put up to 10 items per collection
+            |inner| {
+                (logical_expr_strat(), inner).prop_map(|(aa, logical_exp)| {
+                    let b = BooleanLiteral { span: Span::empty(0), value: true };
+                    let alloc = Allocator::with_capacity(32000);
+                    let right: Expression = Expression::LogicalExpression(Box::new_in(aa, &alloc));
+                    let operator: LogicalOperator = LogicalOperator::Or;
+                    let span: Span = Span::empty(0);
+                    let a = LogicalExpression { left: logical_exp, right, operator, span };
+
+                    Expression::LogicalExpression(Box::new_in(a, &alloc))
+                })
+            },
+        )
+    }
+    /*
+
+    fn nested_logical_expr_strat() -> impl Strategy<Value = Json> {
+        let leaf = prop_oneof![
+            bool_lit_strat(),
+            any::<bool>().prop_map(Json::Bool),
+            any::<f64>().prop_map(Json::Number),
+            ".*".prop_map(Json::String),
+        ];
+        leaf.prop_recursive(
+          8, // 8 levels deep
+          256, // Shoot for maximum size of 256 nodes
+          10, // We put up to 10 items per collection
+          |inner| prop_oneof![
+              // Take the inner strategy and make the two recursive cases.
+              prop::collection::vec(inner.clone(), 0..10)
+                  .prop_map(Json::Array),
+              prop::collection::hash_map(".*", inner, 0..10)
+                  .prop_map(Json::Map),
+          ])
+    }
+     */
+
+    // test that AST -> codegen -> AST roundtrips
+    proptest! {
+            #[test]
+            fn doesnt_crash(cond in nested_logical_expr_strat()) {
+
+              //  let alloc = Allocator::new(); // ugh no
+
+                //let ad = a.
+                //let mut program = ret.program;
+                let mut codegen = CodeGenerator::new();
+                codegen.print_expression(&cond);
+                let s: String = codegen.into_source_text();
+    println!("{}", s);
+           //     assert_eq!(s.as_str(), "false || false");
+            }
+        }
+}
